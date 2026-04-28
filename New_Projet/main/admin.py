72283419cb005100f2050a95f9a35f7c1720c9c4
audrey25eli-moi <audreymissinzuguem@gmail.com>
2026-04-28 15:34:47 +0200

    Correction admin et settings

--- New_Projet/main/admin.py
+++ New_Projet/main/admin.py
@@ -4,6 +4,4 @@ from .models import Produit, RelevePrix
 # On enregistre les modèles pour qu'ils apparaissent dans l'interface admin
 admin.site.register(Produit)
 admin.site.register(RelevePrix)
-from django.contrib import admin
-from .models import Collecte des prix du marché
-admin.site.register(Collecte des prix du marché)
+
